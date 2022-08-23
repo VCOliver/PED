c947d26610ab685e8c985393c9346c1d666f8187
Victor Cruz <92812122+VCOliver@users.noreply.github.com>
2022-08-23 14:47:15 -0300

    Update comp2.vhd

--- comp2.vhd
+++ comp2.vhd
@@ -13,7 +13,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity comp2 is
     Port ( 
            A  : in std_logic_vector (3 downto 0);
-           Ab : out std_logic_vector (3 downto 0)
+           comp2 : out std_logic_vector (3 downto 0)
          );
 end comp2;
 
@@ -21,10 +21,20 @@ architecture Behavioral of comp2 is
   
   signal comp1 : std_logic_vector(3 downto 0);
   signal i     : std_logic_vector(3 downto 0) := "0001";
+  signal j : std_logic;
+
+  component somador_4bits
+      Port(
+          Av : in std_logic_vertor(3 downto 0);
+          Bv : in std_logic_vector(3 downto 0);
+          Cin : in std_logic
+          Sv : out std_logic_vector(3 downto 0);
+          Cout : out std_logic);
+   end component;
    
 begin
   
   comp1 <= not A;
-  Ab <= (comp1 + i);
+  comp2 : somador_4bits port map(Av => comp1, Bv => i, Cin => '0', Sv => comp2, Cout => j);
        
 end Behavioral;
