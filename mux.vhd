843c482072a55d5141c655f2ea38bf9e498574a3
Victor Cruz <92812122+VCOliver@users.noreply.github.com>
2022-08-22 21:44:42 -0300

    Create mux.vhd

---
+++ mux.vhd
@@ -0,0 +1,31 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity mux is
+    Port ( A : in STD_LOGIC_VECTOR (3 downto 0);
+           B : in STD_LOGIC_VECTOR (3 downto 0);
+           Sel : in STD_LOGIC;
+           S : out STD_LOGIC_VECTOR (3 downto 0));
+           
+end mux;
+
+architecture Behavioral of mux is
+
+begin
+
+
+with Sel select
+  
+  S  <= A when '0',
+  B when others;
+
+end Behavioral;
