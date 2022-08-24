b37bc0e4eb94c69bc7f0ec635301985952ffce8c
Victor Cruz <92812122+VCOliver@users.noreply.github.com>
2022-08-24 08:38:08 -0300

    Update som_sub_completo.vhd

--- som_sub_completo/som_sub_completo.vhd
+++ som_sub_completo/som_sub_completo.vhd
@@ -24,7 +24,7 @@ end som_sub_completo;
 architecture Behavioral of som_sub_completo is
 
 
-Component somador_4bits -- Somador de um bit
+Component somador_4bits -- Instanciating the 4 bits adder
     Port (
            Av   : in STD_LOGIC;
            Bv   : in STD_LOGIC;
@@ -34,7 +34,7 @@ Component somador_4bits -- Somador de um bit
            
 end Component;
 
-component mux 
+component mux -- Instanciating the Multiplexer
     Port ( A   : in STD_LOGIC_VECTOR (3 downto 0);
            B   : in STD_LOGIC_VECTOR (3 downto 0);
            Sel : in STD_LOGIC;
@@ -42,32 +42,32 @@ component mux
           
 end component;
 
-component comp2 
+component comp2     -- Instanciating the 2's Complement
     Port ( A  : in STD_LOGIC_VECTOR (3 downto 0);
            comp2 : out STD_LOGIC_VECTOR (3 downto 0));
 end component;
 
-component detector_overflow 
+component detector_overflow     -- Instanciating the Overflow detector
     Port ( Sign_A : in STD_LOGIC;
            Sign_B : in STD_LOGIC;
            Sign_S : in std_logic;
            S : out STD_LOGIC);
 end component;
 
-signal compB, M : std_logic_vector(3 downto 0);
+signal compB, M : std_logic_vector(3 downto 0); -- Auxiliary signals
   
 begin   
 
 
-    Sel_LED <= '1' -- Mostra onde o Switch para operação negativa está
+    Sel_LED <= '1' -- Turns on the led showing where the "subtraction operation" Switch is
 
-    comp: comp2 port map (A => B, comp2 => compB); -- complemento de 2
+    comp: comp2 port map (A => B, comp2 => compB); -- 2's Complement
 
-    mux1: mux port map (A => B, B => compB, Sel => Sel, S => M ); --multiplexador
+    mux1: mux port map (A => B, B => compB, Sel => Sel, S => M ); --multiplexer
   
     -- Saída
-    sum:  somador_4bits port map( Av => A, Bv => M, Cin => Cin, Sv => S, Cout => Cout);
+    sum:  somador_4bits port map( Av => A, Bv => M, Cin => Cin, Sv => S, Cout => Cout); -- Sum
 
-    detec: detector_dverflow port map (Sign_A => A[3], Sign_B => M[3], Sign_S => S[3], S => OVF); --Detector de Overflow
+    detec: detector_overflow port map (Sign_A => A[3], Sign_B => M[3], Sign_S => S[3], S => OVF); -- Overflow Detector
 
 end Behavioral;
