735c268725a3cd2f8c7dbdfa8489892d102922cb
Victor Cruz <92812122+VCOliver@users.noreply.github.com>
2022-09-12 09:19:28 -0300

    Update som_sub_completo.vhd

--- som_sub_completo/som_sub_completo.vhd
+++ som_sub_completo/som_sub_completo.vhd
@@ -14,7 +14,8 @@ entity som_sub_completo is
     Port ( A   : in STD_LOGIC_VECTOR (3 downto 0);
            B   : in STD_LOGIC_VECTOR (3 downto 0);
            Cin : in STD_LOGIC;
-           Sel : in std_logic;
+           Sel1 : in std_logic;
+           Sel  : in std_logic;
            Sel_LED : out std_logic; -- LED to show where select button is
            S   : out STD_LOGIC_VECTOR (3 downto 0);
            Cout: out std_logic;
@@ -54,20 +55,24 @@ component detector_overflow     -- Instanciating the Overflow detector
            S : out STD_LOGIC);
 end component;
 
-signal compB, M : std_logic_vector(3 downto 0); -- Auxiliary signals
+signal compBa, compBb, N, M : std_logic_vector(3 downto 0); -- Auxiliary signals
   
 begin   
 
 
     Sel_LED <= '1' -- Turns on the led showing where the "subtraction operation" Switch is
+    
+    comp_2: comp port map (A => A, comp2 => compBa); -- 2's Complement
 
-    comp: comp2 port map (A => B, comp2 => compB); -- 2's Complement
+    comp: comp2 port map (A => B, comp2 => compBb); -- 2's Complement
 
-    mux1: mux port map (A => B, B => compB, Sel => Sel, S => M ); --multiplexer
+    mux1: mux port map (A => A, B => compBa, Sel1 => Sel, S => M ); --multiplexer
+        
+    mux2: mux port map (A => B, B => compBb, Sel => Sel, S => N ); --multiplexer
   
     -- Saída
-    sum:  somador_4bits port map( Av => A, Bv => M, Cin => Cin, Sv => S, Cout => Cout); -- Sum
+    sum:  somador_4bits port map( Av => M, Bv => N, Cin => Cin, Sv => S, Cout => Cout); -- Sum
 
-    detec: detector_overflow port map (Sign_A => A(3), Sign_B => M(3), Sign_S => S(3), S => OVF); -- Overflow Detector
+    detec: detector_overflow port map (Sign_A => M(3), Sign_B => N(3), Sign_S => S(3), S => OVF); -- Overflow Detector
 
 end Behavioral;
